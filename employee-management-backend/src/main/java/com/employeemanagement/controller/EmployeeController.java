49e1c2f5f1f099b771b5de67e34928503e3dd3cc
Your Name <d.hariharan1405@gmail.com>
2024-11-09 15:19:24 +0530

    initial project setup

---
+++ employee-management-backend/src/main/java/com/employeemanagement/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.employeemanagement.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.employeemanagement.model.Employee;
+import com.employeemanagement.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+		
+		
+	}
+
+}
